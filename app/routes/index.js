eb6dfcf74ea7de266f38efc10eafa66e73befad2
Micaela Ramos <micaeelar@gmail.com>
2022-03-09 20:04:31 +0000

    Routes and controllers tutorial

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
+  model() {
+    const tasks = [{
+      title: "Task A",
+      description: "Make breakfast",
+    }, {
+      title: "Task B",
+      description: "Pay rent",
+    }];
+
+    return tasks;
+  }
 }
