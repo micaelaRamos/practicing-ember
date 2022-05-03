af2399565ed77160a917656486ea1a8bac425392
micaelaRamos <micaela.ramos@storyful.com>
2022-05-03 09:57:56 +0100

    Add integration/rendering tests

--- tests/integration/components/product/detail-test.js
+++ tests/integration/components/product/detail-test.js
@@ -1,26 +1,37 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | product/detail', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it displays data correctly', async function(assert) {
+    this.set('price', {
+      original: 50,
+      current: 30,
+    });
 
-    await render(hbs`<Product::Detail />`);
+    this.set('onChangeColor', function(color) {
+      assert.equal(color, 'red', 'Correct color selected');
+    });
 
-    assert.dom(this.element).hasText('');
+    this.set('isDetail', true);
 
-    // Template block usage:
-    await render(hbs`
-      <Product::Detail>
-        template block text
-      </Product::Detail>
-    `);
+    this.set('colors', [{
+      color: 'red',
+    }]);
 
-    assert.dom(this.element).hasText('template block text');
+    await render(hbs`<Product::Detail
+      @price={{this.price}}
+      @onChangeColor={{this.onChangeColor}}
+      @isDetail={{this.isDetail}}
+      @colors={{this.colors}}
+      />`);
+    
+    assert.dom('[data-test-original-price]').hasText('$50.00');
+    assert.dom('[data-test-current-price]').hasText('$30.00');
+    
+    await click('[data-test-color-selector]');
   });
 });
